c5fdfc1f2fb179817cb53b917e6111d1ca12085b
Noah Wall <104470013+ncwall01@users.noreply.github.com>
2022-08-03 14:17:37 -0400

    Add files via upload

--- TESTmail.php
+++ TESTmail.php
@@ -12,5 +12,5 @@ $recipient = "YOUREMAIL@HERE.COM";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
